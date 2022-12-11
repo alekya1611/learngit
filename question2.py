c1f54c1c4815a520dcd7003c5afe49a16cc21b35
sri alekya <srialekya1611@gmail.com>
2022-12-11 18:46:33 +0530

    some changes

--- question2.py
+++ question2.py
@@ -1,4 +1,13 @@
-car={"suzuki":"red","ford":"white","maruthi":"grey"}
+brand1=input()
+brand2=input()
+brand3=input()
+color1=input()
+color2=input()
+color3=input()
+car={brand1:color1,brand2:color2,brand3:color3}
 x=open("ans.txt","a")
+for k,v in car.items():
+    print(k,v)
+    print(k,v,file=x)
 print(car)
-print("ans:",car,file=x)
+print(car,file=x)
